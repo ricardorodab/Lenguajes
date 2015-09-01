0c67d7a13bd544607ebcca9d2794809ad437b4bd
lzamudio <lzamudio@sumawebdiseno.com>
2015-09-01 10:46:34 -0500

    "test" de funciones

--- README.txt
+++ README.txt
@@ -2,6 +2,6 @@ Integrantes:
 
 Rodríguez Abreu José Ricardo - ricardo_rodab@ciencias.unam.mx - 309216139
 Martínez Martínez Julio César - jc.msaint@gmail.com -
-Zamudio Cervante Luis Enrique - luis.zamudio90@gmail.com -
+Zamudio Cervantes Luis Enrique - luis.zamudio90@gmail.com - 307293136
 # lenguajes20161_RodriguezAbreuJoseRicardo
 Este es un repositorio para la clase de Lenguajes con la profesora Karla del semestre 2016-1
--- practica1/Practica1.rkt
+++ practica1/Practica1.rkt
@@ -87,6 +87,7 @@
                        (cons (car lst) l)) 
                      aux) aux))))
 
+
 ;FUNCIONES AUXILIARES: 
 ;Función auxiliar para generar la cantidad de elementos que tiene una lista sumando uno por uno sus elementos.
 
@@ -106,3 +107,99 @@
      (cond [(= 1 b) #t]
         [(integer? (/ a b)) #f]
         [else (divisor a (- b 1))]))
+
+
+
+; Pruebas
+
+; Función pow
+
+(test (pow 130 3)  2197000)
+(test (pow 23 2)  529)
+(test (pow 15 3)  3375)
+(test (pow 2 5)  32)
+(test (pow 200 0)  1)
+
+
+; Función average
+
+(test (average '(5))  5)
+(test (average '(1 2 3 4 5))  3)
+(test (average '(5 9 1)) 5 )
+(test (average '( 0 0 0))  0)
+(test (average '(5 5 5 5 5))  5)
+
+; Función primes
+
+(test (primes 30)  '(2 3 5 7 11 13 17 19 23 29))
+(test (primes 2)  '(2))
+(test (primes 1)  '())
+(test (primes 100)  '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97))
+(test (primes 24)  '(2 3 5 7 11 13 17 19 23))
+
+; Función zip
+
+(test (zip '() '())  '() )
+(test (zip '(1 ) '())  '() )
+(test (zip '() '(1))  '() )
+(test (zip '(1 2 3) '(4 5 6)) '((1 4) (2 5) (3 6)))
+(test (zip '(1 3) '(2 4)) '((1 2) (3 4)) )
+
+
+; Función reduce
+
+(test (reduce + '(1 2 3 4 5 6 7 8 9 10)) 55)
+(test (reduce pow '(2 2 2)) 16) 
+(test (reduce zip '((1 2 3) (4 5 6) (7 8 9)) ) '((1 (4 7)) (2 (5 8)) (3 (6 9))) )
+(test (reduce * '(2 2)) 4)
+(test (reduce mconcat '((1 2 3) (4 5 6)) ) '(1 2 3 4 5 6) )
+
+
+
+; Función mconcat
+
+(test (mconcat '() '()) '() )
+(test (mconcat '() '(1)) '(1) )
+(test (mconcat '(1) '()) '(1) )
+(test (mconcat '(1 3 5) '(2 4 6)) '(1 3 5 2 4 6) )
+(test (mconcat '(1 ) '(1)) '(1 1) )
+
+
+; Función mmap
+
+(test (mmap  primes '(1 2 3 4 5) ) '(() (2) (2 3) (2 3) (2 3 5)))
+(test (mmap  average '((1) ( 1 2 3 4 5)) ) '(1 3))
+(test (mmap  mpowerset '( () (1 2)) ) '((()) ((1 2) (1) (2) ())) )
+(test (mmap  first '((1 2 3) (2 5 8)) ) '(1 2))
+(test (mmap  add1 '(1 2 3 4 5) ) '(2 3 4 5 6) )
+
+
+; Función mfilter
+
+(test (mfilter (lambda (x) (not (zero? x))) '(2 0 1 4 0)) '(2 1 4))
+(test (mfilter (lambda (l) (not (empty? l))) '((1 4 2) () (2 4) ())) '((1 4 2) (2 4)))
+(test (mfilter (lambda (n) (= (modulo n 2) 0)) '(1 2 3 4 5 6)) '(2 4 6))
+(test (mfilter (lambda (n) (not (= (modulo n 2) 0))) '(1 2 3 4 5 6)) '(1 3 5))
+(test (mfilter (lambda (n) (list? n)) '(1 '() (2 3) 4 5 6)) '('() (2 3)))
+
+; Función any?
+(test (any? number? '()) #f)
+(test (any? number? '(a b c d 1)) #t)
+(test (any? symbol? '(1 2 3 4)) #f)
+(test (any? (lambda (n) (list? n))'(1 '() (2 3) 4 5 6)) #t)
+(test (any? (lambda (x) (not (zero? x))) '(0 0 0 0)) #f)
+
+; Función every?
+(test (every? number? '()) #t)
+(test (every? number? '(1 2 3 4)) #t)
+(test (every? symbol? '(1 2 d 3 4)) #f)
+(test (every? (lambda (n) (list? n))'(1 '() (2 3) 4 5 6)) #f)
+(test (every? (lambda (x) (not (zero? x))) '(0 0 0 0)) #f)
+
+; Función mpowerset
+
+(test (mpowerset '()) '(()))
+(test (mpowerset '(1)) '((1) ()))
+(test (mpowerset '(1 2 )) '((1 2) (1) (2) ()))
+(test (mpowerset '(1 2 3 4)) '((1 2 3 4) (1 2 3) (1 2 4) (1 2) (1 3 4) (1 3) (1 4) (1) (2 3 4) (2 3) (2 4) (2) (3 4) (3) (4) ()))
+(test (mpowerset '(1 2 3 5)) '((1 2 3 5) (1 2 3) (1 2 5) (1 2) (1 3 5) (1 3) (1 5) (1) (2 3 5) (2 3) (2 5) (2) (3 5) (3) (5) ()))
