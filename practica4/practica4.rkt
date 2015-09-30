a1f93cbe895fe244f03a5c7f43beb25ce1c90510
ricardorodab <ricardorodab19@gmail.com>
2015-09-29 21:07:28 -0500

    Practica-4 ejercicio 1 de 4

---
+++ practica4/practica4.rkt
@@ -0,0 +1,76 @@
+#lang plai
+
+(require "practica4-base.rkt")
+
+(print-only-errors true)
+
+; Ejercicio desugar
+
+(define (desugar expr)
+  (type-case FAES expr
+    [numS (n) (num n)]
+    [idS (name) (id name)]
+    [funS (params body) (fun (params (desugar body)))]
+    [appS (fuS ars) (app ((desugar fuS) (dameFAE ars)))] ;(desugar args)?
+    [binopS (f l r) (binop f (desugar l) (desugar r))]
+    [withS (bindings body) (app (fun (sacaName bindings) (desugar body)) (sacaVal bindings))]
+    [with*S (bindings body) (app (fun (sacaName bindings) (desugar body)) (sacaVal bindings))]))
+
+
+(test (desugar (parse '{+ 3 4})) (binop + (num 3) (num 4)))
+(test (desugar (parse '{+ {- 3 4} 7})) (binop + (binop - (num 3) (num 4)) (num 7)))
+(test (desugar (parse '{with {{x {+ 5 5}}} x})) (app (fun '(x) (id 'x)) (list (binop + (num 5) (num 5))) ))
+
+(define (cparse sexp)
+  (desugar (parse sexp)))
+
+(define (interp expr env)
+  ;; Implementar interp
+  (error 'interp "Not implemented"))
+
+(define (rinterp expr)
+  (interp expr (mtSub)))
+
+(test (rinterp (cparse '3)) (numV 3))
+(test (rinterp (cparse '{+ 3 4})) (numV 7))
+(test (rinterp (cparse '{+ {- 3 4} 7})) (numV 6))
+(test (rinterp (cparse '{with {{x {+ 5 5}}} {+ x x}})) (numV 20))
+(test (rinterp (cparse '{with {{x 5}} {+ x x}})) (numV 10))
+(test (rinterp (cparse '{with {{x {+ 5 5}}} {with {{y {- x 3}}} {+ y y}}})) (numV 14))
+(test (rinterp (cparse '{with {{x 5} {y {- 5 3}}} {+ x y}})) (numV 7))
+(test (rinterp (cparse '{with {{x 5}} {+ x {with {{x 3}} 10}}})) (numV 15))
+(test (rinterp (cparse '{with {{x 5}} {+ x {with {{x 3}} x}}})) (numV 8))
+(test (rinterp (cparse '{with {{x 5}} {+ x {with {{y 3}} x}}})) (numV 10))
+(test (rinterp (cparse '{with {{x 5}} {with {{y x}} y}})) (numV 5))
+(test (rinterp (cparse '{with {{x 5}} {with {{x x}} x}})) (numV 5))
+(test (rinterp (cparse '{{fun {x} x} 3})) (numV 3))
+(test (rinterp (cparse '{{{fun {x} x} {fun {x} {+ x 5}}} 3})) (numV 8))
+(test (rinterp (cparse '{with {{x 3}} {fun {y} {+ x y}}})) (closureV '(y) (binop + (id 'x) (id 'y)) (aSub 'x (numV 3) (mtSub))))
+(test (rinterp (cparse '{with {{x 10}} {{fun {y} {+ y x}} {+ 5 x}}})) (numV 25))
+(test (rinterp (cparse '{with {{x 1} {y 2} {z 3}} {+ {+ x y} z}})) (numV 6))
+(test (rinterp (cparse '{{fun {x y z} {+ {+ x y} z}} 1 2 3})) (numV 6))
+(test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}} {z {+ x y}}} z})) (numV 8))
+(test (rinterp (cparse '{with* {{x 3} {y {+ 2 x}} {x 10} {z {+ x y}}} z})) (numV 15))
+(test/exn (rinterp (cparse '{with {{x 10} {x 20}} x})) "El id x está repetido")
+(test (rinterp (cparse '{with* {{x 10} {x 20}} x})) (numV 20))
+
+
+; FUNCIONES AUXILIARES
+
+
+(define (dameFAE lst)
+  (if (empty? lst)
+      '()
+      (cons (desugar (car lst)) (dameFAE (cdr lst)))))
+
+(define (sacaName lst)
+  (if (empty? lst)
+      '()
+      (type-case Binding (car lst)
+        [bind (name val) (cons name (sacaName (cdr lst)))])))
+
+(define (sacaVal lst)
+  (if (empty? lst)
+      '()
+      (type-case Binding (car lst)
+        [bind (name val) (cons (desugar val) (sacaVal (cdr lst)))])))
